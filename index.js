d030da841c4256ffd6f63d2926e11cdbddb5dac8
TobBot2 <38795143+TobBot2@users.noreply.github.com>
2021-04-28 13:42:20 -0400

    Fixed Port Issue
    changed the port to be 3000 only if process.env.PORT is not available.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log("listening at 3000"));
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening at ${port}`));
 app.use(express.static('public'));
